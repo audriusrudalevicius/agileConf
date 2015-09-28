90a11c6c5a699718e735df2c140df5361a83cceb
arudalevicius <arudalevicius@nfq.lt>
2015-09-28 22:09:21 +0300

    token

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+npm install -g jspm
 jspm config registries.github.auth 882b9846a385f451ff0b828804f9d1c22313a8a5
 jspm registry create bower jspm-bower-endpoint -y
 jspm install -y
