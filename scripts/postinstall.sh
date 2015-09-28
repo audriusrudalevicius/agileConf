90fda355be7865597610dcde8628bcbb22b72480
arudalevicius <arudalevicius@nfq.lt>
2015-09-28 22:14:41 +0300

    missing command

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,7 @@
 
 set -e
 npm install -g jspm
+npm install -g jspm-bower-endpoint
 jspm config registries.github.auth 882b9846a385f451ff0b828804f9d1c22313a8a5
 jspm registry create bower jspm-bower-endpoint -y
 jspm install -y
