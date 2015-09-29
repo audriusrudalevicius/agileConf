3999fb26819efa9a6d6adc7d7395ec676cebe9d6
arudalevicius <arudalevicius@nfq.lt>
2015-09-29 13:15:07 +0300

    ts preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,8 @@ module.exports = function(config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel']
+      'src/**/*.js': ['babel'],
+      'src/**/*.ts': ['typescript']
     },
     'babelPreprocessor': {
       options: {
@@ -46,6 +47,27 @@ module.exports = function(config) {
           "es7.decorators",
           "es7.classProperties"
         ]
+      }
+    },
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options: {
+        experimentalDecorators: true,
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: true, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatinateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      // extra typing definitions to pass to the compiler (globs allowed)
+      typings: [
+        'src/typings/tsd.d.ts'
+      ],
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.ts$/, '.js');
       }
     },
 
