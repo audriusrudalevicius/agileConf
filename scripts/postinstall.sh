85319168de643f2400b79a7601ff787574649db4
Audrius <d3trax@users.noreply.github.com>
2015-09-29 11:41:46 +0300

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-set -e
-JSPM_GITHUB_AUTH_TOKEN=${VARIABLE:=GITHUB_AUTH_TOKEN}
 npm install -g jspm
 npm install -g jspm-bower-endpoint
-jspm cc
-jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
+jspm config registries.github.auth $GITHUB_AUTH_TOKEN
 jspm registry create bower jspm-bower-endpoint -y
 jspm install -y
 tsd install
